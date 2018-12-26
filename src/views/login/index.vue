9830a7b716e98f2c1cabf77439f418639276cad8
unknown <wangkang06@ppdai.com>
2018-12-26 15:49:27 +0800

    修改登录后跳转

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -91,7 +91,7 @@ export default {
             .dispatch("Login", this.loginForm)
             .then(() => {
               this.loading = false;
-              this.$router.push({ path: "/" });
+              this.$router.push({ path: "/rule/preselection" });
             })
             .catch(() => {
               this.loading = false;
